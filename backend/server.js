50e019f8771f243c7217cd77cdb260d479e6e955
Rishita Shaw <rishitashaw49@gmail.com>
2023-02-10 20:14:33 +0530

    fix

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,14 @@ const app = express();
 
 app.use(express.json()); // to accept json data
 
-app.use(cors({
-  origin: 'https://www.generalsemantic.com',
-}))
+// app.use(cors({
+//   origin: 'https://www.generalsemantic.com',
+// }))
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("API Running!");
